f9fad4f111869a6ec4dfbdb7c2ffe130835c320d
FantasmasDe <fantasmas.dev@gmail.com>
2023-08-13 20:46:53 +0300

    Четвёртый коммит

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
 fun main(args: Array<String>) {
-    println()
-    println(1 === 0x01)
-    val archiveScreen = Archive()
-    println(archiveScreen.archiveList)
-    archiveScreen.moveTo()
-    println(archiveScreen.command)
+    val screen = Screen.Archive
+    screen.navigateTo(screen)
 }
 
